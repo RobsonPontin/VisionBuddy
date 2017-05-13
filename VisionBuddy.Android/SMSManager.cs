90231392d7101beb06dc2fde31e8715cd9e9f8fe
Robson <rp.pontin@gmail.com>
2017-05-13 12:14:21 -0700

    Vison buddy tests
    First tests with xamarin cross-platform

---
+++ VisionBuddy.Android/SMSManager.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Database;
+
+namespace VisionBuddy.Droid
+{
+    class SMSManager
+    {
+        private const string URI_INBOX = "content://sms/inbox";
+        private const string URI_SENT = "content://sms/sent";
+        private const string URI_DRAFT = "content://sms/draft";
+
+        public class SMSMessage
+        {
+            public string Body { get; set; }
+            public string Address { get; set; }
+            public int ID { get; set; }
+        }
+
+        public List<SMSMessage> SMSItems = new List<SMSMessage>();
+        
+        public enum SMSType
+        {
+            Inbox,
+            Sent,
+            Draft
+        }
+
+        public void GetSMSMessages(SMSType type)
+        {
+            SMSItems.Clear();
+
+            try
+            {
+                string messageURI = null;
+
+                switch (type)
+                {
+                    case SMSType.Inbox:
+                        messageURI = URI_INBOX;
+                        break;
+                    case SMSType.Sent:
+                        messageURI = URI_SENT;
+                        break;
+                    case SMSType.Draft:
+                        messageURI = URI_DRAFT;
+                        break;
+                }
+
+                Android.Net.Uri inboxURI = Android.Net.Uri.Parse(messageURI);
+
+                // list of required columns
+                string[] reqCols = new string[] { "_id", "address", "body" };
+
+                ICursor icursor = Application.Context.ContentResolver.Query(
+                    Android.Net.Uri.Parse(messageURI), reqCols, null, null, null);
+
+                if (icursor != null && icursor.Count > 0)
+                {
+                    for (icursor.MoveToFirst(); !icursor.IsAfterLast; icursor.MoveToNext())
+                    {
+                        SMSMessage item = new SMSMessage();
+                        item.Body = icursor.GetString(icursor.GetColumnIndex("body"));
+                        item.Address = icursor.GetString(icursor.GetColumnIndex("address"));
+                        item.ID = icursor.GetInt(icursor.GetColumnIndex("_id"));
+                        SMSItems.Add(item);
+                    }
+                }
+            }
+            catch
+            { }
+        }
+    }
+}
---
+++ VisionBuddy/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VisionBuddy.Droid;
+using Xamarin.Forms;
+
+namespace VisionBuddy
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+            btLoadSMS.Clicked += BtLoadSMS_Clicked;
+                        
+        }
+
+        private void BtLoadSMS_Clicked(object sender, EventArgs e)
+        {
+            SMSManager smsManager = new SMSManager();
+            smsManager.GetSMSMessages(SMSManager.SMSType.Sent);
+
+            List<string> smsMsgs = new List<string>();
+
+            foreach (var item in smsManager.SMSItems)
+            {
+                smsMsgs.Add(item.ID.ToString());
+                smsMsgs.Add(item.Body);
+                smsMsgs.Add(item.Address);
+            }
+            lvDisplay.ItemsSource = smsMsgs;
+                
+        }
+    }
+}
